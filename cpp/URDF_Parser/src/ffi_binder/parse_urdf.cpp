c0272423b0368f1e689b06cedf2f80f1bf408c2a
Rocky14683 <rocky10208@gmail.com>
2025-05-19 18:54:55 -0400

    add handler caches for rust code cuz ffi is slower

--- cpp/URDF_Parser/src/ffi_binder/parse_urdf.cpp
+++ cpp/URDF_Parser/src/ffi_binder/parse_urdf.cpp
@@ -6,6 +6,8 @@
 #include "urdf/common.h"
 #include "urdf/model.h"
 
+#define GET_MODEL(handle) *static_cast<std::shared_ptr<urdf::UrdfModel> *>(handle)
+
 extern "C" {
 void urdf_try() {
     std::println("hello from urdf_try cpp23 bind!");
@@ -26,9 +28,34 @@ void urdf_free(URDFRobotHandle handle) {
 }
 
 int urdf_visual_count(URDFRobotHandle handle) {
-    auto model = *static_cast<std::shared_ptr<urdf::UrdfModel> *>(handle);
+    auto model = GET_MODEL(handle);
     auto base_link = model->getLink("base_link");
     assert(base_link && "base_link not found");
     return static_cast<int>(base_link->visuals.size());
 }
+
+const char* urdf_get_link_name(URDFRobotHandle handle, int index) {
+    auto model = GET_MODEL(handle);
+    auto base_link = model->getLink("base_link");
+    assert(base_link && "base_link not found");
+    assert(index >= 0 && index < base_link->visuals.size());
+
+    //! this work because rust code will take the ownership of the pointer
+    return base_link->name.c_str();
+}
+
+int urdf_get_geometry_type(URDFRobotHandle handle, int index) {
+    auto model = GET_MODEL(handle);
+    auto base_link = model->getLink("base_link");
+    assert(base_link && "base_link not found");
+    assert(index >= 0 && index < base_link->visuals.size());
+
+    const auto& visual = base_link->visuals[index];
+    if (visual->geometry.has_value()) {
+        return static_cast<int>(visual->geometry.value()->type);
+    }
+    return -1;  // Or an error code
+}
+
+
 } // extern C block
